473bd7c7e69f1bd3757aeca1fed1c551fbc8226d
MaximeGuiliani <maxime.guiliani@hotmail.fr>
2022-03-12 14:46:14 +0100

    correction bug streamer sans calendrier

--- appProject/src/app/services/streamer.service.ts
+++ appProject/src/app/services/streamer.service.ts
@@ -6,21 +6,21 @@ export class StreamerService {
   streamersSubject = new Subject<any[]>();
   private streamers = [
     {
-      streamerName: 'Domingo',
+      streamerName: 'Maxime0',
       streamerProfileImage:
         'https://www.motorsinside.com/images/photo/article/f12022/ferrari-f1-75_jpg_sponsor_00001-sanslogo.jpg',
       isPartner: false,
       id: 0,
     },
     {
-      streamerName: 'Doigby',
+      streamerName: 'Maxime1',
       streamerProfileImage:
         'https://www.monaco-tribune.com/wp-content/uploads/2022/02/scuderia-ferrari.jpeg',
       isPartner: false,
       id: 1,
     },
     {
-      streamerName: 'Maghla',
+      streamerName: 'Maxime2',
       streamerProfileImage:
         'https://static.lexpress.fr/medias_11761/w_1000,h_563,c_fill,g_north/v1519313107/la-sf71-h-presentee-par-l-ecurie-ferrari-le-22-fevrier-2018_6021998.jpg',
       isPartner: false,
